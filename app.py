8edd32999b4261502b5dc8a0e585413603204dd9
shivakumarkomuravelly <shivakumarkomuravelly@gmail.com>
2022-09-29 14:57:01 +0530

    python is added

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello Docker for python"
+
+
+if __name__ == "__main__":
+    app.run(port=8000, host="0.0.0.0")
